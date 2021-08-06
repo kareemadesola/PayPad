ec5c230a76b86e5aec455d6ab4719a6ebf89224c
kareemadesola <kareemadesola1999@gmail.com>
2021-08-06 09:08:43 +0100

    remove setUpActionBarWithNavController- not in use

--- app/src/main/java/com/example/paypad/MainActivity.kt
+++ app/src/main/java/com/example/paypad/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.example.paypad
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.paypad.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
